d47160318ffcc08a26eaa9642f2e010da9e43253
Harjot Singh <Jashanpreet3322jp@gmail.com>
2021-02-11 17:37:39 +1300

    update

--- BookMark2/DatabaseScripts/data.sql
+++ BookMark2/DatabaseScripts/data.sql
@@ -1,4 +1,5 @@
-﻿SET IDENTITY_INSERT [dbo].[PersonalBookMark] ON 
+﻿
+SET IDENTITY_INSERT [dbo].[PersonalBookMark] ON 
 INSERT [dbo].[PersonalBookMark] ([Id], [Title], [BookType], [BookGenre], [BookSubject], [CurrentChapters], [CurrentPage]) VALUES (5, N'Johns adventure', N'Manga', N'adventure', N'Story', 34, 50)
 INSERT [dbo].[PersonalBookMark] ([Id], [Title], [BookType], [BookGenre], [BookSubject], [CurrentChapters], [CurrentPage]) VALUES (6, N'My Repair Guide', N'Learning', N'non-fiction', N'Mechanical', 21, 34)
 INSERT [dbo].[PersonalBookMark] ([Id], [Title], [BookType], [BookGenre], [BookSubject], [CurrentChapters], [CurrentPage]) VALUES (7, N' Cars', N'Comedy', N'fiction', N'Story', 67, 12)
